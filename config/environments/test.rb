1171e8ffbf68b7c508c32da779cb3b6035f4ef22
Kevin Fitzpatrick <kfitzpatrick@Kevin-Fitzpatricks-MacBook-Pro.local>
2009-12-17 12:00:36 -0500

    Removing ZenTest gem from the test environment.
    It's not necessary for the app, just a development tool

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,7 +26,6 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
-config.gem 'ZenTest', :version => '>=4.1.4'
 config.gem 'mocha', :version => '>=0.9.5'
 config.gem 'rspec', :lib => false, :version => '>= 1.2.9'
 config.gem 'rspec-rails', :lib => false, :version => '>= 1.2.9'
